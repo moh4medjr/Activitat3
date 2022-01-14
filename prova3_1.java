3839f6c9dec814aa703a85dffd5fad1f2ff9b4a7
moh4medjr <96302895+moh4medjr@users.noreply.github.com>
2022-01-14 18:13:13 +0100

    Update prova3_1.java

--- prova3_1.java
+++ prova3_1.java
@@ -2,9 +2,9 @@
 class prova3_1 {
     public static void main(String[] args) {
         final String dayName = "Monday";
-        final String monthName = "October";
+        final String mName = "October";
         final String day = "18";
         final String year = "2021";
-        System.out.println(dayName+ " the "+ day+ " of "+ monthName+", "+ year);
+        System.out.printf(dayName+ " the "+ day+ " of "+ monthName+", "+ year);
         }
 }
