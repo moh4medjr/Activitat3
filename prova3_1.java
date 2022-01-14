38d0082000e1279aa3b6f61cae04e570b929b93d
moh4medjr <96302895+moh4medjr@users.noreply.github.com>
2022-01-14 18:19:33 +0100

    Update prova3_1.java

--- prova3_1.java
+++ prova3_1.java
@@ -1,10 +1,6 @@
 # Activitat3
-class prova3_1 {
+class HelloWorldApp {
     public static void main(String[] args) {
-        final String dayName = "Monday";
-        final String monthName = "October";
-        final String day = "18";
-        final String year = "2021";
-        System.out.print(dayName+ " the "+ day+ " of "+ monthName+", "+ year);
-        }
+        System.out.println("Hello World!");
+      }
 }
