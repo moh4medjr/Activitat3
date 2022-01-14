f1742580150f6a13d3577d04e7986824d9fd6455
moh4medjr <96302895+moh4medjr@users.noreply.github.com>
2022-01-14 17:58:40 +0100

    Update prova3_1.java

--- prova3_1.java
+++ prova3_1.java
@@ -1,7 +1,7 @@
 # Activitat3
 class prova3_1 {
     public static void main(String[] args) {
-        final String dayName = "Monday";
+        final String dName = "Monday";
         final String monthName = "October";
         final String day = "18";
         final String year = "2021";
